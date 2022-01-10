2c25d4b191014bdc3addfd7aea33547f8802e195
Viram Jain <viram.jain@kaleyra.com>
2022-01-10 16:33:00 +0530

    added env

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/joho/godotenv v1.4.0
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,20 +3,39 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"video-chat-app/server"
 
+	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
 
 func main() {
 	server.AllRooms.Init()
+	err := godotenv.Load()
+	if err != nil {
+		zap.L().Error("Error in loading env file",
+			zap.Any("Error:", err))
+	}
 
 	http.HandleFunc("/create", server.CreateRoomRequestHandler)
 	http.HandleFunc("/join", server.JoinRoomRequestHandler)
-	log.Println("Starting server on port 8000")
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		zap.L().Error("Error in starting server", zap.Any("Error:", err))
+
+	port := os.Getenv("PORT")
+	if !(port == "") {
+		log.Println("Starting server on port" + port)
+		err := http.ListenAndServe(":"+port, nil)
+		if err != nil {
+			zap.L().Error("Error in starting server",
+				zap.Any("Error:", err))
+		}
+	} else {
+		log.Println("Starting server on port 8000")
+		err := http.ListenAndServe(":8000", nil)
+		if err != nil {
+			zap.L().Error("Error in starting server",
+				zap.Any("Error:", err))
+		}
 	}
 }
