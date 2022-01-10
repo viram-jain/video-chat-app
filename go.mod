11abbb9cbeffd38a7b3ae49d066468637e310d11
Viram Jain <viram.jain@kaleyra.com>
2022-01-10 15:02:05 +0530

    configs added

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
-module github.com/viramjainkaleyra/video-chat-app
+module video-chat-app
 
 go 1.17
 
-require go.uber.org/zap v1.19.1
-
 require (
 	github.com/gorilla/websocket v1.4.2
+	go.uber.org/zap v1.20.0
+)
+
+require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
--- server/rooms.go
+++ server/rooms.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"time"
 
+	"video-chat-app/constant"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -40,7 +42,7 @@ func (r *RoomMap) CreateRoom() string {
 	defer r.Mutex.Unlock()
 
 	rand.Seed(time.Now().UnixNano())
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	var letters = []rune(constant.CharactersForRune)
 	b := make([]rune, 8)
 
 	for i := range b {
